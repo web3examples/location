a3af6e7a83065d293e14ab735b4722d48ab297ef
gpersoon <mail@gpersoon.com>
2019-09-17 15:22:57 +0200

    logo

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -69,6 +69,16 @@ module.exports = {
        skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
     },
 
+     goerli: {
+       provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`), // /YOUR-PROJECT-ID
+       network_id: 5,       // goerli's id
+       gas: 5500000,        // goerli has a lower block limit than mainnet
+       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+       skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+
     // Useful for private networks
     // private: {
       // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
